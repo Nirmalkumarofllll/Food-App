ec134bdce2ecf91310bb50ceeb4dd3a42916cb25
NirmalKumar <nirmalkumarofllll@gmail.com>
2024-10-15 13:52:35 +0530

    update index.js

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,13 @@ const admin = require("firebase-admin");
 require("dotenv").config();
 const serviceAccountKey = require("./serviceAccountKey.json");
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
-// Body parser for our JSON data
+// Middleware
+app.use(cors({ origin: true }));
 app.use(express.json());
-
-// Cross-origin resource sharing (CORS)
-const cors = require("cors");
-app.use(cors({origin: true}));
-app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  next();
-});
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey),
@@ -25,9 +20,9 @@ app.get("/", (req, res) => {
 });
 
 const userRoute = require("./routes/user");
-app.use("/api/users", userRoute);
-
 const productRoute = require("./routes/products");
-app.use("/api/products/", productRoute);
 
-exports.app = functions.https.onRequest(app);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+
+module.exports = app;
