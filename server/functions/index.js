7494f041b625949b94a498cebe2ea399113317a0
NirmalKumar <nirmalkumarofllll@gmail.com>
2024-11-06 13:23:02 +0530

    Update index.js

--- server/functions/index.js
+++ server/functions/index.js
@@ -2,40 +2,45 @@ const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-require('dotenv').config();
-
-const serviceAccountKey = require('./serviceAccountKey.json');
-
 const express = require("express");
-const app = express();
-
-// Body parser for our JSON data
-app.use(express.json());
-
-// Cross-origin resource sharing (CORS)
+require('dotenv').config();
 const cors = require("cors");
-app.use(cors({ origin: true }));
-app.use((req, res, next) => {
-    res.set("Access-Control-Allow-Origin", "*");
-    next();
-});
 
 // Firebase credentials
+const serviceAccountKey = require('./serviceAccountKey.json');
+
+// Initialize Firebase Admin
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountKey)
+  credential: admin.credential.cert(serviceAccountKey)
 });
 
-// API endpoints
+// Initialize Express app
+const app = express();
+
+// CORS configuration to allow requests only from your client domain
+const corsOptions = {
+  origin: 'https://nk-food-app.web.app',  // Allow requests only from this domain
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Authorization', 'Content-Type'],
+};
+
+app.use(cors(corsOptions));  // Apply CORS for all routes
+
+// Body parser middleware for handling JSON data
+app.use(express.json());
+
+// Define routes
 app.get("/", (req, res) => {
-    return res.send('hello world');
+  return res.send('Hello World');
 });
 
-// User routes
+// Include your API route for users
 const userRoute = require('./routes/user');
-app.use("/api/users", userRoute);  
+app.use("/api/users", userRoute);
 
+// Include your API route for products
 const productRoute = require("./routes/products");
-app.use("/api/products/", productRoute);
+app.use("/api/products", productRoute);
 
-// Export the app
+// Export the app as Firebase Function
 exports.app = functions.https.onRequest(app);
